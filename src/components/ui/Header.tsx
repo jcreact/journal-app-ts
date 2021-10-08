c374739b8b514075541421706bffb08d136d2486
José Carlos Palma <palmahn@gmail.com>
2021-10-08 14:07:02 -0600

    fix(ui): Alineación de elementos del Appbar.

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,21 +1,36 @@
-import { AppBar, Toolbar, Typography, IconButton, Tooltip } from '@mui/material';
+import { AppBar, Toolbar, Typography, IconButton, Tooltip, Box, Stack } from '@mui/material';
 import JournalIcon from '@mui/icons-material/ArticleOutlined';
 import LogoutIcon from '@mui/icons-material/Logout';
 
 export const Header = () => {
     return (
-        <AppBar enableColorOnDark position="static">
+        <AppBar
+            enableColorOnDark
+            position="fixed"
+            sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        >
             <Toolbar>
-                <JournalIcon />
-                <Typography variant="h6" ml={1} mr={2}>
-                    Journal App
-                </Typography>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                        width: '100%',
+                    }}
+                >
+                    <Stack direction="row" alignItems="center">
+                        <JournalIcon />
+                        <Typography variant="h6" ml={1} mr={2}>
+                            Journal App
+                        </Typography>
+                    </Stack>
 
-                <Tooltip title="Cerrar sesión">
-                    <IconButton>
-                        <LogoutIcon color="secondary" />
-                    </IconButton>
-                </Tooltip>
+                    <Tooltip title="Cerrar sesión">
+                        <IconButton>
+                            <LogoutIcon color="secondary" />
+                        </IconButton>
+                    </Tooltip>
+                </Box>
             </Toolbar>
         </AppBar>
     );
