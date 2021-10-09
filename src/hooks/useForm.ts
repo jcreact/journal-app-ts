be9f081d4b5e6ec927316fa7c117f1da8ae0b809
José Carlos Palma <palmahn@gmail.com>
2021-10-09 03:23:49 -0600

    feat(hooks): Implementación de touched para los atributos.

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,7 +1,10 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useState, FormEvent } from 'react';
 
 export const useForm = <FormType>(initialState: FormType) => {
     const [values, setValues] = useState(initialState);
+    const [touched, setTouched] = useState(
+        Object.keys(initialState).reduce((acc, key) => ({ ...acc, [key]: false }), {} as any)
+    );
 
     const reset = () => {
         setValues(initialState);
@@ -12,11 +15,33 @@ export const useForm = <FormType>(initialState: FormType) => {
             ...values,
             [target.name]: target.value,
         });
+
+        setTouched({
+            ...touched,
+            [target.name]: true,
+        });
+    };
+
+    const touchedAll = () =>
+        setTouched(Object.keys(touched).reduce((acc, key) => ({ ...acc, [key]: true }), {} as any));
+
+    const toucheMe = (ev: FormEvent<HTMLFormElement>) => {
+        ev.preventDefault();
+        const { name } = ev.target as HTMLInputElement | HTMLTextAreaElement;
+        setTouched({ ...touched, [name]: true });
+    };
+
+    const checkMail = (email: string): boolean => {
+        return /^[A-Za-z0-9ñÑ._]+@[a-z0-9.-]{2,}\.[a-z]{2,}$/.test(email);
     };
 
     return {
         values,
         handleInputChange,
         reset,
+        touched,
+        touchedAll,
+        toucheMe,
+        checkMail,
     };
 };
