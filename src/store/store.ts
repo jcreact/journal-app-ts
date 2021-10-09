b2f0d263cbcda170aa924a23fc756f8d9a626b55
José Carlos Palma <palmahn@gmail.com>
2021-10-08 23:51:07 -0600

    feat(store): Configuración de thunk al  store.

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,9 @@
-import { createStore, combineReducers, StoreEnhancer } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
 import { authReducer } from '../reducers/authReducer';
 
-// Typeguard para utilizar Redux DevTools con TS
-type WindowWithDevTools = Window & {
-    __REDUX_DEVTOOLS_EXTENSION__: () => StoreEnhancer<unknown, {}>;
-};
-
-const isReduxDevtoolsExtenstionExist = (
-    arg: Window | WindowWithDevTools
-): arg is WindowWithDevTools => {
-    return '__REDUX_DEVTOOLS_EXTENSION__' in arg;
-};
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // Listado de reducer de la aplicación.
 const reducers = combineReducers({
@@ -18,7 +11,4 @@ const reducers = combineReducers({
 });
 
 // Creación del store global de la aplicación.
-export const store = createStore(
-    reducers,
-    isReduxDevtoolsExtenstionExist(window) ? window.__REDUX_DEVTOOLS_EXTENSION__() : undefined
-);
+export const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
